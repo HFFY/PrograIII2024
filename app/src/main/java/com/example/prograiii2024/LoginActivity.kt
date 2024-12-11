7e09e803bcb1a7e04d988f9c3cb98a72f9d0ce57
hugoflores <hugorilahffy@gmail.com>
2024-12-11 10:19:28 -0400

    Ejemplo de implementacion de login

---
+++ app/src/main/java/com/example/prograiii2024/LoginActivity.kt
@@ -0,0 +1,33 @@
+package com.example.prograiii2024
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.prograiii2024.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        auth = Firebase.auth
+    }
+
+    fun loginUsuario(){
+        auth.signInWithEmailAndPassword("email","password")
+            .addOnCompleteListener(this) { respuesta ->
+                
+            }
+    }
+}
